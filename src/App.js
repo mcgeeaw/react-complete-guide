db5b907991cef718e2516d63f322c6e9e00836e3
McgeeAW <Wayne316!>
2018-09-13 12:29:34 -0500

    Adding state to project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+        { name: 'Adam', age: 35},
+        { name: 'Joey', age :25},
+        { name: 'Jim', age : 23}
+    ],
+      otherState: 'something else'
+  }
+
+  switchNameHandler = () => {
+    //console.log("CLICK");
+     // NO this.state.persons[0].name = 'YOLO';
+      this.setState({persons: [
+              { name: 'Yolo', age: 35},
+              { name: 'Joey', age :25},
+              { name: 'Jim', age : 99}
+          ]})
+  }
+
   render() {
      return (
        <div className="App">
          <h1>Hi, I'm a React App</h1>
+         <p>This is working JSX</p>
+         <button onClick={this.switchNameHandler}>Switch Name</button>
+         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+           <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Swimming</Person>
+           <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
        </div>
      );
     //  return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
